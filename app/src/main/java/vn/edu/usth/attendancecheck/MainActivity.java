087831905c51353fb2555424dc5110133199c0c3
letuss004 <letuss004@gmail.com>
2021-12-27 15:50:31 +0700

    QR scanner success

--- app/src/main/java/vn/edu/usth/attendancecheck/MainActivity.java
+++ app/src/main/java/vn/edu/usth/attendancecheck/MainActivity.java
@@ -1,18 +1,30 @@
 package vn.edu.usth.attendancecheck;
 
+import android.Manifest;
 import android.os.Bundle;
 import android.view.View;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-import vn.edu.usth.attendancecheck.databinding.ActivityMainBinding;
+import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+import pub.devrel.easypermissions.EasyPermissions;
+import vn.edu.usth.attendancecheck.databinding.ActivityMainBinding;
+import vn.edu.usth.attendancecheck.ui.CameraFragment;
+import vn.edu.usth.attendancecheck.ui.CurrentClassesFragment;
+import vn.edu.usth.attendancecheck.ui.HistoryFragment;
+import vn.edu.usth.attendancecheck.ui.HomeFragment;
+
+public class MainActivity extends AppCompatActivity
+        implements EasyPermissions.PermissionCallbacks {
     private ActivityMainBinding binding;
 
     @Override
@@ -21,14 +33,86 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
+        // methods
+        navigationSetup();
+    }
 
-        // navigation setup
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
+    }
+
+    @Override
+    public void onPermissionsGranted(int requestCode, @NonNull List<String> perms) {
+
+    }
+
+
+    /*
+    -------------------------------------------private----------------------------------------
+     */
+
+    /**
+     * todo: back stack is not deploy,
+     */
+    private void navigationSetup() {
         BottomNavigationView navigationView = binding.botNav;
         NavHostFragment navHostFragment =
                 (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
         assert navHostFragment != null;
         NavController navController = navHostFragment.getNavController();
         NavigationUI.setupWithNavController(navigationView, navController);
+
+        // view on click
+        navigationView.setOnItemSelectedListener(
+                item -> {
+                    int id = item.getItemId();
+                    int current = navigationView.getSelectedItemId();
+                    if (id == R.id.navIcon_home) {
+                        switchFragment(new HomeFragment());
+                    } else if (id == R.id.navIcon_checkList) {
+                        switchFragment(new CurrentClassesFragment());
+                    } else if (id == R.id.navIcon_qr) {
+                        openCameraRequirePermission();
+                    } else if (id == R.id.navIcon_history) {
+                        switchFragment(new HistoryFragment());
+                    } else if (id == R.id.navIcon_menu) {
+
+                    }
+                    return true;
+                }
+        );
+    }
+
+    private void openCameraRequirePermission() {
+        String[] perm = {Manifest.permission.CAMERA, Manifest.permission.READ_EXTERNAL_STORAGE};
+        if (EasyPermissions.hasPermissions(this, perm)) {
+            switchFragment(new CameraFragment());
+        } else {
+            EasyPermissions.requestPermissions(
+                    this,
+                    "Need permission beacause of something",
+                    123,
+                    perm
+            );
+        }
+    }
+
+    private void switchFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.nav_host_fragment, fragment);
+        transaction.commit();
+    }
+
+
+    @Override
+    public void onPermissionsDenied(int requestCode, @NonNull List<String> perms) {
+        if (EasyPermissions.somePermissionPermanentlyDenied(this, perms)) {
+
+        }
     }
 
 }
--- app/src/main/java/vn/edu/usth/attendancecheck/ui/CameraFragment.java
+++ app/src/main/java/vn/edu/usth/attendancecheck/ui/CameraFragment.java
@@ -1,7 +1,9 @@
 package vn.edu.usth.attendancecheck.ui;
 
+import android.Manifest;
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,6 +16,11 @@ import androidx.fragment.app.Fragment;
 import com.budiyev.android.codescanner.CodeScanner;
 import com.budiyev.android.codescanner.CodeScannerView;
 
+import java.util.List;
+
+import pub.devrel.easypermissions.AfterPermissionGranted;
+import pub.devrel.easypermissions.AppSettingsDialog;
+import pub.devrel.easypermissions.EasyPermissions;
 import vn.edu.usth.attendancecheck.databinding.FragmentCameraBinding;
 
 /**
@@ -21,12 +28,13 @@ import vn.edu.usth.attendancecheck.databinding.FragmentCameraBinding;
  * Use the {@link CameraFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class CameraFragment extends Fragment {
-    private CodeScanner mCodeScanner;
+public class CameraFragment extends Fragment
+        implements EasyPermissions.PermissionCallbacks {
+    private static final String TAG ="CameraFragment" ;
     private View view;
     private FragmentCameraBinding binding;
-    private final Activity activity = getActivity();
-    private final CodeScannerView scannerView = binding.scannerView;
+    private CodeScanner mCodeScanner;
+
 
     public CameraFragment() {
         // Required empty public constructor
@@ -49,12 +57,14 @@ public class CameraFragment extends Fragment {
 //            mParam1 = getArguments().getString(ARG_PARAM1);
 //            mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
     }
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container,
                              Bundle savedInstanceState) {
+        requireCameraPermisson();
         binding = FragmentCameraBinding.inflate(inflater, container, false);
         view = binding.getRoot();
         return view;
@@ -64,21 +74,75 @@ public class CameraFragment extends Fragment {
     public void onViewCreated(@NonNull View view,
                               @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        final Activity activity = getActivity();
+        final CodeScannerView scannerView = binding.scannerView;
         assert activity != null;
         //
         mCodeScanner = new CodeScanner(activity, scannerView);
         mCodeScanner.setDecodeCallback(
-                (result) -> activity.runOnUiThread(
-                        () -> Toast.makeText(activity, result.getText(), Toast.LENGTH_SHORT).show()
-                )
+                (result) -> {
+                    Thread thread = new Thread(
+                            () -> Log.e(TAG, "onViewCreated: " + result.getText())
+                    );
+                    thread.start();
+                }
         );
         scannerView.setOnClickListener(
                 v -> mCodeScanner.startPreview()
         );
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        mCodeScanner.startPreview();
+    }
+
+    @Override
+    public void onPause() {
+        mCodeScanner.releaseResources();
+        super.onPause();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
+    }
+
+    @Override
+    public void onPermissionsGranted(int requestCode, @NonNull List<String> perms) {
+
+    }
+
+    @Override
+    public void onPermissionsDenied(int requestCode, @NonNull List<String> perms) {
+        if (EasyPermissions.somePermissionPermanentlyDenied(this, perms)) {
+            new AppSettingsDialog.Builder(this)
+                    .build()
+                    .show();
+        }
+    }
+
     /*
 
      */
+    @AfterPermissionGranted(123)
+    private void requireCameraPermisson() {
+        String[] perm = {Manifest.permission.CAMERA, Manifest.permission.READ_EXTERNAL_STORAGE};
+        if (EasyPermissions.hasPermissions(getContext(), perm)) {
+            Toast.makeText(getContext(), "Open camera", Toast.LENGTH_SHORT).show();
+        } else {
+            EasyPermissions.requestPermissions(
+                    this,
+                    "Need permission beacause of something",
+                    123,
+                    perm
+            );
+        }
+    }
+
 
 }
